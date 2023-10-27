5e0ed7a4dfdc1753b3343e1f10dadc080d7818ea
shenal2001 <149025975+shenal2001@users.noreply.github.com>
2023-10-27 13:26:13 +0530

    Create addoption.php

---
+++ final_project/PHP code/addoption.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0″>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include_once '';
+        
+    ?>
+    <main>
+        <div class= "card">
+            <div class="image">
+                <img src="45 Best Large Dog Breeds for People Who Have a Lot of Love to Give.jpeg">
+</div>
+<div class ="caption">
+    <p class="dog_name"><b>Dog Name</b> </p>
+    <p class="age"><b> Leesha</b></p>
+</div>
+</div>
+</main>
+</body>
+</html>
+
